305c123b6998c94d1975282bce056d3ce95456ac
Nathan Rousselet <nathan@agence-webup.com>
2022-05-06 00:16:00 +0200

    Add(script/style): connected script & index style

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,28 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, price, tagline/*, id*/ } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const pLocation = document.createElement( 'p' );
+        const pTagline = document.createElement( 'p' );
+        const pPrice = document.createElement( 'p' );
+        img.setAttribute("src", picture)
+        pLocation.setAttribute("class", "pLocation")
+        pTagline.setAttribute("class", "pTagline")
+        pPrice.setAttribute("class", "pPrice")
         h2.textContent = name;
+        pLocation.textContent = city + ', ' + country;
+        pTagline.textContent = tagline;
+        pPrice.textContent = price + '€/jour';
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(pLocation);
+        article.appendChild(pTagline);
+        article.appendChild(pPrice);
         return (article);
     }
     return { name, picture, getUserCardDOM }
